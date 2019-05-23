5e83fe275d1265e8c8df9eb37586a6f8c5013cc2
observateur1980 <44187905+observateur1980@users.noreply.github.com>
2019-05-23 00:06:03 -0700

    may 23 12:05 am

--- home/urls.py
+++ home/urls.py
@@ -10,6 +10,10 @@ urlpatterns = [
     
     
     path('', views.Home.as_view(), name='home'),
+    path('about', views.About.as_view(), name='about'),
+    path('services', views.Services.as_view(), name='services'),
+    path('events', views.Events.as_view(), name='events'),
+    path('contact', views.Contact.as_view(), name='contact'),
     
    
 ]
